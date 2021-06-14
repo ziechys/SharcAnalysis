93b5791234538bfabdbe6edf04951a2df7498be8
Karl Michael <km.z@gmx.de>
2021-06-14 12:46:26 +0200

    call python3

--- diagnostics.py
+++ diagnostics.py
@@ -17,13 +17,11 @@
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
-#
+# 
 #    You should have received a copy of the GNU General Public License
 #    inside the SHARC manual.  If not, see <http://www.gnu.org/licenses/>.
 #
 #******************************************
-
-#!/usr/bin/env python2
 
 from copy import deepcopy 
 import math
